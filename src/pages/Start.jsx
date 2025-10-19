a283b74cee5f688ad9d90214d465af095971df2b
Raouf <raoufghzl6@gmail.com>
2025-10-20 00:05:26 +0100

    Fix import case and refactor Label props

--- src/pages/Start.jsx
+++ src/pages/Start.jsx
@@ -1,25 +1,34 @@
 import { useState } from "react";
-import Label from "../components/Label";
+import Label from "../components/label";
+import Return from "../components/Return";
 
 export default function Start() {
   const [name, setName] = useState(["", ""]);
-  const [color, setColor] = useState(["red", "green"]);
 
+  const [color, setColor] = useState(["red", "green"]);
   return (
-    <div
-      id="lobby"
-      className="h-screen bg-[url('/frogBackground.png')] bg-cover bg-no-repeat bg-bottom"
-    >
-      <div className="absolute flex justify-evenly items-center h-full w-full">
+    <div id="lobby">
+      <div
+        style={{
+          zIndex: "99",
+          position: "absolute",
+          top: "2rem",
+          left: "2rem",
+        }}
+        className="shadow-xl/20"
+      >
+        <Return prev="" />
+      </div>
+      <div className="absolute flex justify-evenly items-center h-full w-full ">
         <Label
-          p="player_1"
+          p={"player_1"}
           name={name}
           setName={setName}
           color={color}
           setColor={setColor}
         />
         <Label
-          p="player_2"
+          p={"player_2"}
           name={name}
           setName={setName}
           color={color}
